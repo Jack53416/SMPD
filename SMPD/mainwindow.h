55bdb28c000d7b54aa799e49aed32cedc9e2daa9
Jacek Byczkiewicz <jck53416@gmail.com>
2017-03-23 16:22:23 +0100

    Czysty program od Ghosta z otwieraniem pliku w classifiers

---
+++ SMPD/mainwindow.h
@@ -0,0 +1,65 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <fstream>
+#include <QCheckBox>
+#include <QFileDialog>
+#include <QtCore>
+#include <QtGui>
+#include <QMessageBox>
+
+#include <QImage>
+#include <QDebug>
+#include <QTextBrowser>
+
+#include"database.h"
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private:
+    bool loadFile(const std::string &fileName);
+    void updateDatabaseInfo(QTextBrowser* textBrowser);
+    void saveFile(const std::string &fileName);
+
+    void FSupdateButtonState(void);
+    void FSsetButtonState(bool state);
+
+
+
+private slots:
+    void on_FSpushButtonOpenFile_clicked();
+
+    void on_FSpushButtonCompute_clicked();
+
+    void on_FSpushButtonSaveFile_clicked();
+
+    void on_PpushButtonSelectFolder_clicked();
+
+
+    void on_CpushButtonOpenFile_clicked();
+
+    void on_CpushButtonSaveFile_clicked();
+
+    void on_CpushButtonTrain_clicked();
+
+    void on_CpushButtonExecute_clicked();
+
+private:
+    Ui::MainWindow *ui;
+
+private:
+     Database database;
+};
+
+#endif // MAINWINDOW_H
